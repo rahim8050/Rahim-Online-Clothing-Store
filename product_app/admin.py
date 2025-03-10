2d58cdd4518b78769e8bf73c6b137e3ad3bfbda9
rahim8050 <rahimranxx8050@gmail.com>
2025-03-10 12:12:35 +0000

    updates

--- product_app/admin.py
+++ product_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Category,Product
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug':('name',)}
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category']
+    prepopulated_fields = {'slug':('name',)}
