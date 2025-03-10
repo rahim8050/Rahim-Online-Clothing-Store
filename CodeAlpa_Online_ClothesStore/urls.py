3d077e418988c82fa578e8d767761a109600a927
rahim8050 <rahimranxx8050@gmail.com>
2025-03-10 08:57:14 +0000

    updates

--- CodeAlpa_Online_ClothesStore/urls.py
+++ CodeAlpa_Online_ClothesStore/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from Main_App import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.index, name='index'),
     path('admin/', admin.site.urls),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
