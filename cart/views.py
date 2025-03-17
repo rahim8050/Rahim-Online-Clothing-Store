b01684a4266187889487b6f4513fcce8f892ee78
rahim8050 <rahimranxx8050@gmail.com>
2025-03-17 09:46:10 +0000

    ins

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,7 @@
       
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
+from django.http import JsonResponse
 
 from product_app.models import Product
 from .models import Cart,CartItem
@@ -18,3 +19,24 @@ def cart_add(request, product_id):
             request.session['cart_id'] = cart.id
 
         product = get_object_or_404(Product, id=product_id)
+        cart_item = CartItem.objects.filter(cart=cart, product=product)
+            
+    if not created:
+        cart_item.quantity += 1
+    
+    cart_item.save()
+            
+    response_data = {
+        "success":True,
+        "message": f'Added {product.name} to cart'
+    }
+    return JsonResponse(response_data)
+
+def cart_detail(request):
+       cart_id =request.session.get('cart_id')
+       if cart_id:
+              cart = get_object_or_404(Cart, id=cart_id)
+              return render(request, 'cart/cart_detail.html', {'cart': cart})
+    
+
+        
