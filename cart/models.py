3d82e3330f9586e9535b7f4b674c9772761f51c5
rahim8050 <rahimranxx8050@gmail.com>
2025-03-17 08:34:26 +0000

    ins

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db.models import Sum
+# Example: Import in another app's views.py
+from product_app.models import Product
 
-# Create your models here.
+class Cart(models.Model):
+   created_at = models.DateTimeField(auto_now_add=True)
+   updated_at = models.DateTimeField(auto_now=True)
+   def get_total_price(self):
+       return self.cart.aggregate(total_price=Sum('price'))['total_price']
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart,related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name="cart_items", on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    def get_total_price(self):
+        return self.product.price * self.quantity
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+      
+        from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from product_app.models import Product
+from .models import Cart,CartItem
+
+@require_POST
+def cart_add(request, product_id):
+    cart_id =request.session.get('cart_id')
+    if cart_id:
+        try:
+            cart = Cart.objects.get(id=cart_id)
+        except Cart.DoesNotExist:
+            cart =Cart.objects.create()
+        else:
+            cart = Cart.objects.create()
+            request.session['cart_id'] = cart.id
+
+        product = get_object_or_404(Product, id=product_id)
