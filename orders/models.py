e0e2d20a1846f310c83989483cfec4da44afd1b2
rahim8050 <rahimranxx8050@gmail.com>
2025-03-20 14:30:52 +0000

    ins

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from product_app.models import Product
+
+
 # Create your models here.
 class Order(models.Model):
     full_name = models.CharField(max_length=100)
@@ -7,3 +10,17 @@ class Order(models.Model):
     address = models.CharField(max_length=250)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name="order_items", on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def get_cost(self):
+        return self.price * self.quantity
