1e980f8bf6d21a57befc01f93431fefa63d83235
rahim8050 <rahimranxx8050@gmail.com>
2025-03-20 17:37:29 +0000

    ins

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from  cart.models import Cart
 # Create your views here.
+def order_create(request):
+    Cart = None
+    cart_id = request.session.get('cart_id')
+    if cart_id:
+        cart = Cart.object.get(id=cart_id)
+
+        if not cart or not cart.items.exists():
+            return redirect('cart:cart_detail')
