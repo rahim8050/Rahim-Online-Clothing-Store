4f97e5b7ce78132405cc3930ea559170b98c2965
rahim8050 <58936541+rahim8050@users.noreply.github.com>
2025-04-10 14:16:11 +0100

    Update settings.py

--- CodeAlpa_Online_ClothesStore/settings.py
+++ CodeAlpa_Online_ClothesStore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-_du59u-xto50=+@9y#wkghc5x8e$29!g$!#t$a@5o3rm9-$*a7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
