6dd4982499ad8556dc8cc2cd68cc4c87e9eea4e8
rahim8050 <rahimranxx8050@gmail.com>
2025-04-11 06:19:50 +0100

    cart

--- cart/views.py
+++ cart/views.py
@@ -7,36 +7,80 @@ from product_app.models import Product
 from .models import Cart,CartItem
 
 @require_POST
+
+
+
+
+
 def cart_add(request, product_id):
+    # Get or create cart with proper session handling
     cart_id = request.session.get('cart_id')
 
+    # 1. Fix session persistence for new carts
     if cart_id:
         try:
             cart = Cart.objects.get(id=cart_id)
         except Cart.DoesNotExist:
+            # Session contained invalid cart ID - create new one and update session
             cart = Cart.objects.create()
+            request.session['cart_id'] = cart.id
+            request.session.modified = True  # Force session save
     else:
+        # No existing cart - create new one
         cart = Cart.objects.create()
         request.session['cart_id'] = cart.id
+        request.session.modified = True  # Force session save
 
+    # 2. Get product with proper error handling
     product = get_object_or_404(Product, id=product_id)
 
-    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+    # 3. Update or create cart item
+    cart_item, created = CartItem.objects.get_or_create(
+        cart=cart,
+        product=product,
+        defaults={'quantity': 1}
+    )
 
     if not created:
         cart_item.quantity += 1
+        cart_item.save()
 
-    cart_item.save()
-
-    response_data = {
+    # 4. Return proper JSON response
+    return JsonResponse({
         "success": True,
-        "message": f'Added {product.name} to cart'
-    }
+        "message": f'Added {product.name} to cart',
+        "cart_total_items": cart.total_items()
+    })
+# def cart_add(request, product_id):
+#     cart_id = request.session.get('cart_id')
+#
+#     if cart_id:
+#         try:
+#             cart = Cart.objects.get(id=cart_id)
+#         except Cart.DoesNotExist:
+#             cart = Cart.objects.create()
+#     else:
+#         cart = Cart.objects.create()
+#         request.session['cart_id'] = cart.id
+#
+#     product = get_object_or_404(Product, id=product_id)
+#
+#     cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+#
+#     if not created:
+#         cart_item.quantity += 1
+#
+#     cart_item.save()
+#
+#     response_data = {
+#         "success": True,
+#         "message": f'Added {product.name} to cart'
+#     }
+#
+#     return JsonResponse(response_data)
 
-    return JsonResponse(response_data)
 
 
-# cart/views.py
 
 
 
