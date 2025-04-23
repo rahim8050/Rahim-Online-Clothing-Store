1d0b02e8ed3ebf42a28d4c8c143daf3366eb77e9
rahim8050 <rahimranxx8050@gmail.com>
2025-04-23 15:24:24 +0100

    env

--- config.py
+++
@@ -1,7 +0,0 @@
-# config.py (excluded via .gitignore)
-import os
-
-MPESA_CREDENTIALS = {
-    "consumer_key": os.getenv("MPESA_CONSUMER_KEY"),
-    "consumer_secret": os.getenv("MPESA_CONSUMER_SECRET"),
-}
