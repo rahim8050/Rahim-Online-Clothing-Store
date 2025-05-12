262b0e9dd903fcec7482e355e320f5ff87019730
rahim8050 <rahimranxx8050@gmail.com>
2025-05-12 10:49:06 +0100

    mpesa

--- Mpesa/views.py
+++ Mpesa/views.py
@@ -1,30 +1,20 @@
-import base64
-import json
-
-import requests
-from dotenv import load_dotenv
-import os
-
-load_dotenv()  # Load variables from .env
-
-consumer_key = os.getenv("MPESA_CONSUMER_KEY")
-consumer_secret = os.getenv("MPESA_CONSUMER_SECRET")
-from django.http import HttpResponse
 from django.shortcuts import render
-
-from django.views.decorators.csrf import csrf_exempt
 from django_daraja.mpesa.core import MpesaClient
-appname = 'mpesa'
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+
+cl = MpesaClient()
 
 # Create your views here.
 
 def trigger(request):
     cl = MpesaClient()
-    phone_number = '254717815133'
+    phone_number = '254769690483'
     amount = 1
     account_reference = '01-ABC'
     transaction_desc = 'Online Clothes Payment'
-    callback_url = 'https://intent-in-katydid.ngrok-free.app/mpesa/callback-handler'
+    callback_url = 'https://4938-41-90-172-81.ngrok-free.app/mpesa/callback-handler'
     response = cl.stk_push(phone_number, amount, account_reference, transaction_desc, callback_url)
     return HttpResponse(response)
 
@@ -38,24 +28,3 @@ def callback(request):
 def mpesa(request):
     return render(request, 'index.html')
 
-def generate_access_token():
-    try:
-        credentials = f"{CONSUMER_KEY}:{CONSUMER_SECRET}"
-        encoded_credentials = base64.b64encode(credentials.encode()).decode()
-
-        headers = {
-            "Authorization": f"Basic {encoded_credentials}",
-            "Content-Type": "application/json",
-        }
-        response = requests.get(
-            f"{MPESA_BASE_URL}/oauth/v1/generate?grant_type=client_credentials",
-            headers=headers,
-        ).json()
-
-        if "access_token" in response:
-            return response["access_token"]
-        else:
-            raise Exception("Access token missing in response.")
-
-    except requests.RequestException as e:
-        raise Exception(f"Failed to connect to M-Pesa: {str(e)}")
