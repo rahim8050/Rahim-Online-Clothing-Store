d338aac6eecf2abd6e9975ff7067129c64e0e10d
rahim8050 <rahimranxx8050@gmail.com>
2025-05-21 16:49:25 +0300

    Implement M-Pesa integration with STK push and callback handling

--- Mpesa/urls.py
+++ Mpesa/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = "Mpesa"
 
 urlpatterns = [
-    path('', views.mpesa, name='mpesa'),
-    path('trigger/', views.trigger, name='trigger-payment'),
-    path('mpesa/callback-handler/', views.callback, name='callback-handler'),
+    # path('', views.mpesa, name='mpesa'),
+    # path('trigger/', views.trigger, name='trigger-payment'),
+    # path('mpesa/callback/', views.callback, name='mpesa-callback'),
+      path('trigger/', views.trigger_stk_push, name='mpesa_trigger'),
+    path('callback/', views.stk_callback, name='mpesa_callback'),
 ]
