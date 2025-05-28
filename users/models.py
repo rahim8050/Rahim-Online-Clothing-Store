fa1b6f3656c3120c72d3c6cd3422fb32e4027ccc
rahim8050 <rahimranxx8050@gmail.com>
2025-05-28 11:00:33 +0300

    Remove Cart and CartItem models along with their associated fields from the database schema.

--- users/models.py
+++ users/models.py
@@ -8,24 +8,24 @@ from product_app.models import Product
 class CustomUser(AbstractUser):
     pass
 
-class Cart(models.Model):
-    user = models.ForeignKey(
-        CustomUser, 
-        on_delete=models.CASCADE, 
-        null=True, 
-        blank=True
-    )
-    session_key = models.CharField(max_length=40, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+# class Cart(models.Model):
+#     user = models.ForeignKey(
+#         CustomUser, 
+#         on_delete=models.CASCADE, 
+#         null=True, 
+#         blank=True
+#     )
+#     session_key = models.CharField(max_length=40, null=True, blank=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
 
-class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    user = models.ForeignKey(
-        CustomUser, 
-        on_delete=models.CASCADE, 
-        null=True, 
-        blank=True
-    )
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
+# class CartItem(models.Model):
+#     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+#     user = models.ForeignKey(
+#         CustomUser, 
+#         on_delete=models.CASCADE, 
+#         null=True, 
+#         blank=True
+#     )
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     quantity = models.PositiveIntegerField(default=1)
