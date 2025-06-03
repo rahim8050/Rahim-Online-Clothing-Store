76a4098bb5617d1bd081dcbb0a459a02bb098b5d
rahim8050 <rahimranxx8050@gmail.com>
2025-06-03 11:37:38 +0300

    Add increment and decrement functionality to cart items and update URLs

--- cart/urls.py
+++ cart/urls.py
@@ -1,5 +1,5 @@
 from django.urls import  path
-from .views import cart_add, cart_detail, cart_remove, cart_count
+from .views import cart_add, cart_detail, cart_remove, cart_count,cart_increment, cart_decrement
 
 app_name = 'cart'
 
@@ -8,5 +8,7 @@ urlpatterns = [
     path('add/<int:product_id>/', cart_add, name='cart_add'),
     path("remove/<int:product_id>/",cart_remove,name='cart_remove'),
     path('cart/count/', cart_count, name='cart_count'),
+    path('increment/<int:product_id>/', cart_increment, name='cart_increment'),
+    path('decrement/<int:product_id>/', cart_decrement, name='cart_decrement'),
 
 ]
