6650bfd5c00ecbf6b435ae6d9b065a4f713875e0
rahim8050 <rahimranxx8050@gmail.com>
2025-06-04 10:19:20 +0300

    Refactor code structure for improved readability and maintainability

--- CodeAlpa_Online_ClothesStore/urls.py
+++ CodeAlpa_Online_ClothesStore/urls.py
@@ -24,7 +24,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                #   path('utilities/', include('utilities.urls')), 
+                  path('utilities/', include('utilities.urls')), 
                   path('cart/', include('cart.urls')),
                   path('orders/', include("orders.urls")),
                   path('mpesa/', include('Mpesa.urls')),
--- utilities/urls.py
+++ utilities/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from utilities import about, contact, terms, privacy
+from utilities.views import about
 app_name = 'utilities'
 urlpatterns = [
     path('about/', about, name='about'),
-    path('contact/', contact, name='contact'),
-    path('terms/', terms, name='terms'),
-    path('privacy/',privacy, name='privacy'),
+    # path('contact/', contact, name='contact'),
+    # path('terms/', terms, name='terms'),
+    # path('privacy/',privacy, name='privacy'),
 ]
