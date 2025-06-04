bb4a8c39a554076f78b7595c16924e6534df8bfb
rahim8050 <rahimranxx8050@gmail.com>
2025-06-04 10:19:20 +0300

    Refactor code structure for improved readability and maintainability

--- product_app/views.py
+++ product_app/views.py
@@ -9,6 +9,7 @@ from product_app.models import Product
 from .models import Category,Product
 # Create your views here.
 def product_list(request,category_slug=None):
+    
     category = None
     products = Product.objects.filter(available=True)
     categories = Category.objects.all()
@@ -21,6 +22,9 @@ def product_list(request,category_slug=None):
         'products': products,
         'categories': categories,
     })
+
+    
+
 def product_detail(request,id,slug):
     product = get_object_or_404(Product,id=id,slug=slug,available=True)
     return render(request,'products/product/detail.html',{
