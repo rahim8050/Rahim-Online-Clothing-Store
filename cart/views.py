9af64027f03a684e8439cfeb0f32383d8d12e431
rahim8050 <rahimranxx8050@gmail.com>
2025-06-10 12:48:15 +0300

    Refactor cart functionality by updating URL patterns, optimizing cart count logic, and enhancing product list display with improved layout and image handling

--- cart/views.py
+++ cart/views.py
@@ -9,34 +9,7 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib import messages
 
 
-# @require_POST
-# def cart_add(request, product_id):
-#     cart_id = request.session.get('cart_id')
-    
-#     if cart_id:
-#         try:
-#             cart = Cart.objects.get(id=cart_id)
-#         except Cart.DoesNotExist:
-#             cart = Cart.objects.create()
-#     else:
-#         cart = Cart.objects.create()
-#         request.session['cart_id'] = cart.id
-    
-#     product = get_object_or_404(Product, id=product_id)
-    
-#     cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
-    
-#     if not created:
-#         cart_item.quantity += 1
-    
-#     cart_item.save()
-    
-#     response_data = {
-#         "success":True,
-#         "message": f'Added {product.name} to cart'
-#     }
-    
-#     return JsonResponse(response_data)
+
 @require_POST
 def cart_add(request, product_id):
     
@@ -77,17 +50,8 @@ def cart_add(request, product_id):
 
 
 def cart_count(request):
-    count = 0
-    cart_id = request.session.get('cart_id')
-
-    if cart_id:
-        try:
-            cart = Cart.objects.get(id=cart_id)
-            count = cart.total_items()
-        except Cart.DoesNotExist:
-            pass
-
-    return JsonResponse({'count': count})
+    cart = Cart(request)
+    return JsonResponse({'count': len(cart)})
 
 
 
@@ -240,11 +204,6 @@ def cart_decrement(request, product_id):
         messages.error(request, "Item not found in cart")
     
     return redirect("cart:cart_detail")
-# def cart_remove(request, product_id):
-#     cart_id = request.session.get('cart_id')
-#     cart = get_object_or_404(Cart, id=cart_id)
-#     item = get_object_or_404(CartItem, id=product_id,cart=cart)
-#     item.delete()
-#     return redirect("cart:cart_detail")
+
 
 
