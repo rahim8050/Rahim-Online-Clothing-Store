e8599251b645e333183e55c76e9127580c39e7d8
rahim8050 <rahimranxx8050@gmail.com>
2025-06-10 11:16:38 +0300

    Refactor user registration and activation process with improved error handling and email activation flow

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from users.views import Login, RegisterUser, Logout,activate
+from users.views import Login, RegisterUser, Logout,activate, profile
 app_name = "users"
 urlpatterns = [
 path("login/",Login.as_view(),name="login"),
 path("register/",RegisterUser.as_view(),name="register"),
 path("logout/", Logout.as_view(), name="logout"),
  path('activate/<uidb64>/<token>/', activate, name='activate'),
+ path('profile/', profile, name='profile'),
 #  path('test-email/', test_email, name='test_email'),
 ]
