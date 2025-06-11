26d58d575bde8dd988247898143d19f6082470fe
rahim8050 <rahimranxx8050@gmail.com>
2025-06-11 16:15:36 +0300

    Add cart context processor and enhance cart functionality; remove email verification module and improve profile page styling

---
+++ cart/context_processors.py
@@ -0,0 +1,14 @@
+from .models import Cart
+
+def cart_counter(request):
+    cart_id = request.session.get('cart_id')
+    count = 0
+
+    if cart_id:
+        try:
+            cart = Cart.objects.get(id=cart_id)
+            count = sum(item.quantity for item in cart.items.all())
+        except Cart.DoesNotExist:
+            count = 0
+
+    return {'cart_total_items': count}
--- emailverification/tokens.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
-class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
-    def _make_hash_value(self, user, timestamp):
-        return f"{user.pk}{user.is_active}{timestamp}"
-account_activation_token = AccountActivationTokenGenerator()
