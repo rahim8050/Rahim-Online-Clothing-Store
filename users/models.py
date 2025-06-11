4394799febc15b3be66a0f04aeb2d8e4461daec9
rahim8050 <rahimranxx8050@gmail.com>
2025-06-11 10:05:11 +0300

    Implement custom authentication backend to allow login with either username or email; update user model to ensure unique email addresses.

--- users/models.py
+++ users/models.py
@@ -1,30 +1,8 @@
 from django.contrib.auth.models import AbstractUser
-
+from django.db import models
 
 
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    pass
-
-# class Cart(models.Model):
-#     user = models.ForeignKey(
-#         CustomUser, 
-#         on_delete=models.CASCADE, 
-#         null=True, 
-#         blank=True
-#     )
-#     session_key = models.CharField(max_length=40, null=True, blank=True)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-
-# class CartItem(models.Model):
-#     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-#     user = models.ForeignKey(
-#         CustomUser, 
-#         on_delete=models.CASCADE, 
-#         null=True, 
-#         blank=True
-#     )
-#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-#     quantity = models.PositiveIntegerField(default=1)
+    email = models.EmailField(unique=True, max_length=191)
--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from users.views import Login, RegisterUser, Logout,activate, profile
+from users.views import Login,Logout,RegisterUser,activate, profile
 app_name = "users"
 urlpatterns = [
 path("login/",Login.as_view(),name="login"),
 path("register/",RegisterUser.as_view(),name="register"),
+#  path('register/', register, name='register'),
 path("logout/", Logout.as_view(), name="logout"),
  path('activate/<uidb64>/<token>/', activate, name='activate'),
  path('profile/', profile, name='profile'),
