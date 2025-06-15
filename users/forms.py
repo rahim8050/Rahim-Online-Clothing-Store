1e764dfdc8f658cfad1181658d541b0424682fe6
rahim8050 <rahimranxx8050@gmail.com>
2025-06-15 13:18:06 +0300

    Add profile update form and enhance profile page layout

--- users/forms.py
+++ users/forms.py
@@ -9,6 +9,7 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
+CustomUser = get_user_model()
 
 class RegisterUserForm(UserCreationForm):
     class Meta:
@@ -56,4 +57,17 @@ class ResendActivationEmailForm(forms.Form):
         except User.DoesNotExist:
             pass
         return email
+ 
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email']
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent'
+            }),
+        }    
 
