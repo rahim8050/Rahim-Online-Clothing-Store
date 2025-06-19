b51fb74fcd3a5feb7f023a498aa490f46fd85873
rahim8050 <rahimranxx8050@gmail.com>
2025-06-19 14:43:56 +0300

    users forgot   their email

--- users/urls.py
+++ users/urls.py
@@ -12,16 +12,9 @@ path("logout/", Logout.as_view(), name="logout"),
  path('profile/', profile_view, name='profile'),
   path('resend-activation/', ResendActivationEmailView.as_view(), name='resend_activation'),
       # Password reset flow
-path(
-    'password_reset/',
-    auth_views.PasswordResetView.as_view(
-        template_name='accounts/password_reset.html',
-        success_url=reverse_lazy('users:password_reset_done')
-    ),
-    name='password_reset'
-),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
+ path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 #  path('test-email/', test_email, name='test_email'),
 ]
