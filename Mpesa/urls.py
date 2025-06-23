1112ad6c236437c525be7cd3556c8fa2b02cd934
rahim8050 <rahimranxx8050@gmail.com>
2025-06-23 16:54:24 +0300

    mpesa completion

--- Mpesa/urls.py
+++ Mpesa/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # path('', views.mpesa, name='mpesa'),
     # path('trigger/', views.trigger, name='trigger-payment'),
     # path('mpesa/callback/', views.callback, name='mpesa-callback'),
+# path('trigger/<int:id>/', views.stk_push, name='mpesa_trigger'),
       path('trigger/', views.trigger_stk_push, name='mpesa_trigger'),
     path('callback/', views.stk_callback, name='mpesa_callback'),
     # path('payment<int:order_id>/',views.mpesa_payment, name='mpesa_payment'),
