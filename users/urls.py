cf4edcbebdbdae58ece0766fb89ed59d59994bb3
rahim8050 <rahimranxx8050@gmail.com>
2025-06-26 16:12:08 +0300

    profile brush off

--- users/urls.py
+++ users/urls.py
@@ -12,10 +12,9 @@ path("logout/", Logout.as_view(), name="logout"),
 path('activate/<uidb64>/<token>/', activate, name='activate'),
 path('profile/', profile_view, name='profile'),
 path('resend-activation/', ResendActivationEmailView.as_view(), name='resend_activation'),
-      # Password reset flowpath('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 path('my-orders/', my_orders, name='my_orders'),
+# password reset flow
+
+
 #  path('test-email/', test_email, name='test_email'),
 ]
