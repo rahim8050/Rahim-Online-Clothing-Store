1ca26170b01c21e2cfa92ab5a435364384e3a6a3
rahim8050 <rahimranxx8050@gmail.com>
2025-07-31 19:14:56 +0300

    paypal conflicts

--- Rahim_Online_ClothesStore/settings.py
+++ Rahim_Online_ClothesStore/settings.py
@@ -164,7 +164,9 @@ STRIPE_WEBHOOK_SECRET = os.getenv("STRIPE_WEBHOOK_SECRET")
 
 # PayPal configuration
 PAYPAL_CLIENT_ID = os.environ.get('PAYPAL_CLIENT_ID')
+print(PAYPAL_CLIENT_ID)
 PAYPAL_CLIENT_SECRET = os.environ.get('PAYPAL_CLIENT_SECRET')
+print(PAYPAL_CLIENT_SECRET)
 PAYPAL_MODE = os.environ.get('PAYPAL_MODE', 'sandbox')
 
 # Paystack configuration
