de60686d20c26b5e8a3c899ed3bd1af36d7d35a3
rahim8050 <58936541+rahim8050@users.noreply.github.com>
2025-08-10 15:51:15 +0300

    docs: add role sync instructions

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import Group
+from django.core.management import call_command
 from django.test import TestCase
 
-# Create your tests here.
+from .roles import (
+    ROLE_ADMIN,
+    ROLE_CUSTOMER,
+    ROLE_DRIVER,
+    ROLE_VENDOR,
+    ROLE_VENDOR_STAFF,
+)
+
+
+class SyncRolesTests(TestCase):
+    def test_idempotent(self):
+        call_command("sync_roles")
+        call_command("sync_roles")
+        roles = [
+            ROLE_ADMIN,
+            ROLE_CUSTOMER,
+            ROLE_VENDOR,
+            ROLE_VENDOR_STAFF,
+            ROLE_DRIVER,
+        ]
+        for role in roles:
+            self.assertEqual(Group.objects.filter(name=role).count(), 1)
+
