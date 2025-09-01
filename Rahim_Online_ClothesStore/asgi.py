e382cb743b6bb5cd740fb5a962749f9b7b151612
rahim8050 <rahimranxx8050@gmail.com>
2025-09-01 06:22:27 +0300

    working drivers and customers pages

--- Rahim_Online_ClothesStore/asgi.py
+++ Rahim_Online_ClothesStore/asgi.py
@@ -1,11 +1,8 @@
-# Rahim_Online_ClothesStore/asgi.py
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
-
-from orders import routing as orders_routing  # contains websocket_urlpatterns
+import orders.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Rahim_Online_ClothesStore.settings")
 
@@ -13,18 +10,5 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AllowedHostsOriginValidator(          # host must be in ALLOWED_HOSTS
-        OriginValidator(                               # origin must be in this allowlist
-            AuthMiddlewareStack(
-                URLRouter(orders_routing.websocket_urlpatterns)
-            ),
-            [
-                "http://127.0.0.1:8000",
-                "http://localhost:8000",
-                "http://localhost:5173",              # Vite dev server, if you open pages from here
-                "https://your-domain.com",
-                "https://www.your-domain.com",
-            ],
-        )
-    ),
+    "websocket": AuthMiddlewareStack(URLRouter(orders.routing.websocket_urlpatterns)),
 })
