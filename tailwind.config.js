647db9542f1dec1b62004e36891e8840b6d65a2d
rahim8050 <rahimranxx8050@gmail.com>
2025-09-18 18:34:41 +0300

    tailwind: track config + input; ignore built CSS only

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",         // project-level templates/
+    "./**/templates/**/*.html",      // app-level users/templates/, orders/templates/, etc.
+    "./static/js/**/*.js",
+    "./static/src/**/*.js",
+    "./src/**/*.{js,ts,vue}",
+    "./**/*.py",                     // optional if you build class strings in Python
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
